a82c0d3f9dd783042888efd712281fd3f0b41c56
bessavagner <bessavagner@gmail.com>
2024-03-19 17:48:12 -0300

    tests: update tests

--- tests/anti_detect_driver/test_execute_scripts.py
+++ tests/anti_detect_driver/test_execute_scripts.py
@@ -18,109 +18,109 @@ class TestFindMethods(BaseTestAntiDetectDriver):
     
     mocker_driver = MockWebdriver()
 
-    # @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
-    # @patch(EXECUTE, new=mocker_driver.execute)
-    # def test_get_by_current_page_referrer(self):
-    #     timeout = 5
-    #     url_1 = "https://example.com"
-    #     url_2 = "https://different-url.com"
+    @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
+    @patch(EXECUTE, new=mocker_driver.execute)
+    def test_get_by_current_page_referrer(self):
+        timeout = 5
+        url_1 = "https://example.com"
+        url_2 = "https://different-url.com"
         
-    #     self.driver.get(url_1)
+        self.driver.get(url_1)
         
-    #     task = threading.Thread(
-    #         target=self.driver.get_by_current_page_referrer,
-    #         args=(url_2,)
-    #     )
-    #     task.start()
-    #     task.join(timeout=timeout)
-    #     if task.is_alive():
-    #         self.fail("Method is stuck in an infinite loop")
-    #     else:
-    #         self.assertTrue(True, "Method completed execution")
+        task = threading.Thread(
+            target=self.driver.get_by_current_page_referrer,
+            args=(url_2,)
+        )
+        task.start()
+        task.join(timeout=timeout)
+        if task.is_alive():
+            self.fail("Method is stuck in an infinite loop")
+        else:
+            self.assertTrue(True, "Method completed execution")
         
-    #     self.assertNotEqual(self.driver.current_url, url_1)
+        self.assertNotEqual(self.driver.current_url, url_1)
 
-    # @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
-    # @patch(EXECUTE, new=mocker_driver.execute)
-    # def test_find_element(self):
-    #     element = self.driver.find_element(by='id', value='id')
-    #     self.assertEqual(element.by, 'css selector')
-    #     self.assertEqual(element.value, '[id="id"]')
+    @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
+    @patch(EXECUTE, new=mocker_driver.execute)
+    def test_find_element(self):
+        element = self.driver.find_element(by='id', value='id')
+        self.assertEqual(element.by, 'css selector')
+        self.assertEqual(element.value, '[id="id"]')
 
-    # @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
-    # @patch(EXECUTE, new=mocker_driver.execute)
-    # def test_find_elements(self):
-    #     element = self.driver.find_elements(by='class name', value='class')
-    #     self.assertIsInstance(element, list)
-    #     self.assertEqual(element[0].by, 'css selector')
-    #     self.assertEqual(element[0].value, '.class')
+    @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
+    @patch(EXECUTE, new=mocker_driver.execute)
+    def test_find_elements(self):
+        element = self.driver.find_elements(by='class name', value='class')
+        self.assertIsInstance(element, list)
+        self.assertEqual(element[0].by, 'css selector')
+        self.assertEqual(element[0].value, '.class')
 
-    # @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
-    # @patch(EXECUTE, new=mocker_driver.execute)
-    # @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
-    # def test_get_element_or_none(self):
-    #     element = self.driver.get_element_or_none('//tag[@attr="value"]', wait=5)
-    #     self.assertEqual(element.by, 'xpath')
-    #     self.assertEqual(element.value, '//tag[@attr="value"]')
+    @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
+    @patch(EXECUTE, new=mocker_driver.execute)
+    @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
+    def test_get_element_or_none(self):
+        element = self.driver.get_element_or_none('//tag[@attr="value"]', wait=5)
+        self.assertEqual(element.by, 'xpath')
+        self.assertEqual(element.value, '//tag[@attr="value"]')
 
-    # @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
-    # @patch(EXECUTE, new=mocker_driver.execute)
-    # @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
-    # def test_get_element_or_none_by_selector(self):
-    #     element = self.driver.get_element_or_none_by_selector('class', wait=5)
-    #     logger.debug(element)
-    #     self.assertEqual(element.by, 'css selector')
-    #     self.assertEqual(element.value, 'class')
+    @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
+    @patch(EXECUTE, new=mocker_driver.execute)
+    @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
+    def test_get_element_or_none_by_selector(self):
+        element = self.driver.get_element_or_none_by_selector('class', wait=5)
+        logger.debug(element)
+        self.assertEqual(element.by, 'css selector')
+        self.assertEqual(element.value, 'class')
 
-    # @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
-    # @patch(EXECUTE, new=mocker_driver.execute)
-    # @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
-    # def test_get_element_by_id(self):
-    #     element = self.driver.get_element_by_id('id', wait=5)
-    #     self.assertEqual(element.by, 'css selector')
-    #     self.assertEqual(element.value, '[id="id"]')
+    @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
+    @patch(EXECUTE, new=mocker_driver.execute)
+    @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
+    def test_get_element_by_id(self):
+        element = self.driver.get_element_by_id('id', wait=5)
+        self.assertEqual(element.by, 'css selector')
+        self.assertEqual(element.value, '[id="id"]')
 
-    # @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
-    # @patch(EXECUTE, new=mocker_driver.execute)
-    # @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
-    # def test_get_element_or_none_by_text_contains(self):
-    #     text = 'mock_text'
-    #     element = self.driver.get_element_or_none_by_text_contains(text, wait=5)
-    #     self.assertEqual(element.by, 'xpath')
-    #     self.assertEqual(element.value, f'//*[contains(text(), "{text}")]')
+    @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
+    @patch(EXECUTE, new=mocker_driver.execute)
+    @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
+    def test_get_element_or_none_by_text_contains(self):
+        text = 'mock_text'
+        element = self.driver.get_element_or_none_by_text_contains(text, wait=5)
+        self.assertEqual(element.by, 'xpath')
+        self.assertEqual(element.value, f'//*[contains(text(), "{text}")]')
 
-    # @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
-    # @patch(EXECUTE, new=mocker_driver.execute)
-    # @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
-    # def test_get_element_or_none_by_text(self):
-    #     text = 'mock_text'
-    #     element = self.driver.get_element_or_none_by_text(text, wait=5)
-    #     self.assertEqual(element.by, 'xpath')
-    #     self.assertEqual(element.value, f'//*[text()="{text}"]')
+    @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
+    @patch(EXECUTE, new=mocker_driver.execute)
+    @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
+    def test_get_element_or_none_by_text(self):
+        text = 'mock_text'
+        element = self.driver.get_element_or_none_by_text(text, wait=5)
+        self.assertEqual(element.by, 'xpath')
+        self.assertEqual(element.value, f'//*[text()="{text}"]')
 
-    # @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
-    # @patch(EXECUTE, new=mocker_driver.execute)
-    # @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
-    # def test_find(self):
-    #     element = self.driver.find('id')
-    #     self.assertEqual(element.by, 'id')
-    #     self.assertEqual(element.value, 'id')
+    @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
+    @patch(EXECUTE, new=mocker_driver.execute)
+    @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
+    def test_find(self):
+        element = self.driver.find('id')
+        self.assertEqual(element.by, 'id')
+        self.assertEqual(element.value, 'id')
 
-    # @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
-    # @patch(EXECUTE, new=mocker_driver.execute)
-    # @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
-    # def test_find_all(self):
-    #     elements = self.driver.find_all('class', by='class name')
-    #     self.assertIsInstance(elements, list)
-    #     self.assertEqual(elements[0].by, 'class name')
-    #     self.assertEqual(elements[0].value, 'class')
+    @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
+    @patch(EXECUTE, new=mocker_driver.execute)
+    @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
+    def test_find_all(self):
+        elements = self.driver.find_all('class', by='class name')
+        self.assertIsInstance(elements, list)
+        self.assertEqual(elements[0].by, 'class name')
+        self.assertEqual(elements[0].value, 'class')
 
-    # @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
-    # @patch(EXECUTE, new=mocker_driver.execute)
-    # @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
-    # def test_find_raises_type_error(self):
-    #     with self.assertRaises(TypeError):
-    #         self.driver.find('class', 'class')  # ''by' is obligatory keyword
+    @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
+    @patch(EXECUTE, new=mocker_driver.execute)
+    @patch(WEBDRIVER_WAIT, new=MockWebDriverWait(mocker_driver, 5).until)
+    def test_find_raises_type_error(self):
+        with self.assertRaises(TypeError):
+            self.driver.find('class', 'class')  # ''by' is obligatory keyword
 
     @patch(EXECUTE_SCRIPT, new=mocker_driver.execute_script)
     @patch(EXECUTE, new=mocker_driver.execute)
