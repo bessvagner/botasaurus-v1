b02130c58286660717800cf0ba07801492d9bb80
bessavagner <bessavagner@gmail.com>
2024-03-19 17:46:25 -0300

    refactor: better format log

--- botasaurus/__init__.py
+++ botasaurus/__init__.py
@@ -9,7 +9,10 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # noqa E501
+formatter = logging.Formatter(
+    "%(levelname)s - function: (%(name)s at %(funcName)s "
+    "line %(lineno)d): %(message)s"
+)
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
