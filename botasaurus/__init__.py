438c0b8910899da1780b1424264c9bfba0278567
bessavagner <bessavagner@gmail.com>
2024-05-31 12:52:00 -0300

    fix: change log config order

--- botasaurus/__init__.py
+++ botasaurus/__init__.py
@@ -6,18 +6,14 @@ from .anti_detect_driver import AntiDetectDriverRemote
 from .anti_detect_requests import AntiDetectRequests
 import botasaurus.bt as bt
 
-logger = logging.getLogger("botasaurus")
-logger.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
     "%(levelname)s - function: (%(name)s at %(funcName)s "
     "line %(lineno)d): %(message)s"
 )
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
 
-logging.getLogger('urllib3').setLevel(logging.ERROR)
-logging.getLogger('selenium').setLevel(logging.WARNING)
-logging.getLogger('asyncio').setLevel(logging.WARNING)
-logging.getLogger('httpcore').setLevel(logging.WARNING)
+logger = logging.getLogger("botasaurus")
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+logger.setLevel(logging.DEBUG)
+logger.addHandler(console_handler)
